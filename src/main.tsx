65a43c3e375ce93d112971dccd15715b45d0c0aa
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-19 14:07:19 +0200

    have buttons strange styling

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
+import {
+	createBrowserRouter,
+	Navigate,
+	RouterProvider,
+} from "react-router-dom";
 import "./index.scss";
 import { PageWelcome } from "./pages/PageWelcome.tsx";
 import { PageAbout } from "./pages/PageAbout.tsx";
 import { Page404 } from "./pages/Page404.tsx";
 import { PagePrimeReact } from "./pages/PagePrimeReact.tsx";
+import { PrimeReactProvider } from "primereact/api";
 
 const router = createBrowserRouter([
 	{
@@ -19,7 +24,11 @@ const router = createBrowserRouter([
 			},
 			{
 				path: "primereact",
-				element: <PagePrimeReact/>
+				element: (
+					<PrimeReactProvider>
+						<PagePrimeReact />
+					</PrimeReactProvider>
+				),
 			},
 			{
 				path: "about",
--- src/pages/PagePrimeReact.tsx
+++ src/pages/PagePrimeReact.tsx
@@ -1,5 +1,17 @@
+import { Button } from "primereact/button";
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+// import "primereact/resources/themes/bootstrap4-light-purple/theme.css";
+
 export const PagePrimeReact = () => {
 	return (
-		<p><a className="underline" href="https://primereact.org">Prime React</a></p>
-	)
-}
+		<>
+			<p>
+				<a className="underline" href="https://primereact.org">
+					Prime React
+				</a>
+			</p>
+			<h2>Buttons</h2>
+			<Button label="Check" icon="pi pi-check" />
+		</>
+	);
+};
