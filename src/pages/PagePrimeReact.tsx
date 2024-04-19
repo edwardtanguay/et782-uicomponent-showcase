efe376e6b7d4fb243c329dabefbd2c8547976dfb
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-19 14:30:53 +0200

    primereact finished

--- src/pages/PagePrimeReact.tsx
+++ src/pages/PagePrimeReact.tsx
@@ -1,16 +1,18 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Dropdown, DropdownChangeEvent } from "primereact/dropdown";
 import { Button } from "primereact/button";
 import "primereact/resources/themes/lara-light-cyan/theme.css";
+import { Slider, SliderChangeEvent } from "primereact/slider";
 // import "primereact/resources/themes/bootstrap4-light-purple/theme.css";
 
 type City = {
 	name: string;
 	code: string;
-}
+};
 
 export const PagePrimeReact = () => {
 	const [selectedCity, setSelectedCity] = useState<City>({} as City);
+	const [boxWidth] = useState<number>(50);
 	const cities = [
 		{ name: "New York", code: "NY" },
 		{ name: "Rome", code: "RM" },
@@ -23,6 +25,12 @@ export const PagePrimeReact = () => {
 	const handleSelectedCity = (e: DropdownChangeEvent) => {
 		const value = e.value;
 		setSelectedCity(value);
+	};
+
+	const handleBoxWidth = (e: SliderChangeEvent) => {
+		const value = e.value;
+		// setBoxWidth(value);
+		console.log("slider value", value);
 	};
 	return (
 		<>
@@ -44,7 +52,23 @@ export const PagePrimeReact = () => {
 					className="w-[20rem] md:w-14rem text-xs"
 				/>
 			</div>
-			{selectedCity.name && <p className="mt-3">You selected: {selectedCity.name} ({selectedCity.code})</p>}
+			{selectedCity.name && (
+				<p className="mt-3">
+					You selected: {selectedCity.name} ({selectedCity.code})
+				</p>
+			)}
+
+			<h2>Sliders</h2>
+			<div className="card flex justify-content-center">
+				<Slider
+					value={boxWidth}
+					onChange={(e) => handleBoxWidth(e)}
+					className="w-14rem"
+					range
+					min={0}
+					max={200}
+				/>
+			</div>
 		</>
 	);
 };
