4571f6d9e0c2ea28ad036d5669f098a9179ace25
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-19 15:59:49 +0200

    responsive

--- src/components/SmartImage.tsx
+++ src/components/SmartImage.tsx
@@ -1,22 +1,23 @@
 interface IProps {
 	id: number;
 	name?: string;
+	attending?: boolean;
 }
 
-export const SmartImage = ({ id, name = "unknown" }: IProps) => {
+export const SmartImage = ({ id, name = "unknown", attending = false }: IProps) => {
 	return (
-		<>
-			<div className="mb-4">
+		<div>
+			<div className={`mb-4 ${!attending && 'opacity-50'}`}>
 				<img
 					src={`images/employee_${id}.jpg`}
-					className={`${name==='unknown' ? 'rounded' : 'rounded-t-lg'} w-[10rem]`}
+					className={`w-full md:w-[10rem] ${name==='unknown' ? 'rounded' : 'rounded-t-lg'}`}
 				/>
 				{name !== "unknown" && (
-					<p className="bg-slate-600 text-white p-1 w-[10rem] rounded-b-lg">
+					<p className="bg-slate-600 text-white p-1 w-full md:w-[10rem] rounded-b-lg">
 						{name}
 					</p>
 				)}
 			</div>
-		</>
+		</div>
 	);
 };
--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -5,10 +5,17 @@ export const PageWelcome = () => {
 		<>
 			<p className="mb-3">This is the welcome page.</p>
 
-			<SmartImage id={1} name="Alice Wonderworld" />
-			<SmartImage id={2} name="Jenny Ashwald" />
-			<SmartImage id={3} />
-
+			<section className="flex flex-col md:flex-row md:gap-3 md:flex-wrap">
+				<SmartImage id={1} attending={true} name="Alice Wonderworld" />
+				<SmartImage id={2} attending={true} name="Jenny Ashwald" />
+				<SmartImage id={3} attending={true} name="Molly Anderson" />
+				<SmartImage id={4} attending={true} name="Jolie Rangers" />
+				<SmartImage id={5} attending={true} name="Horace Ankers" />
+				<SmartImage id={6} attending={true} name="Jack Dangar" />
+				<SmartImage id={7} attending={true} name="Ben Quander" />
+				<SmartImage id={8} attending={true} name="Ashley Nonteen" />
+				<SmartImage id={9} attending={true} name="Grace Gendar" />
+			</section>
 		</>
 	);
 };
